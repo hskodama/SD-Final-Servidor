f8be518154aa98cf655a697867e8c6e8f98f2ed5
Shinki <hskodama@gmail.com>
2019-01-30 19:46:30 -0200

    final

--- cloudbroker.py
+++ cloudbroker.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson import json_util
+from pprint import pprint
 import json
+import bson
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb://admin:sd1234@ds159184.mlab.com:59184/sd"
@@ -22,7 +24,8 @@ def init_provedor(pid):
             'ram': data['ram'],
             'hd': data['hd'],
             'preco':data['preco'],  
-            'usando': ['False']
+            'usando': 'False',
+            'reserva': '0'
         })
 
     return jsonify({'Ok': True})
@@ -30,17 +33,12 @@ def init_provedor(pid):
 @app.route('/provedor/search/<pid>', methods=['POST'])
 def search_provedor(pid):
     data = request.get_json()
-    retorno = {}
     busca = mongo.db['vm'].find(
         {
-            'pid': [pid]
+            'pid': pid
         }
     )
-
-    for resultados in busca:
-        json_data = json.dumps(resultados)
-
-    return json_data
+    return bson.json_util.dumps(busca) 
 
 @app.route('/search',methods=['POST'])
 def search_vm():
@@ -50,11 +48,46 @@ def search_vm():
             'vcpu':{'$gte': data['vcpu']},
             'ram': {'$gte': data['ram']},
             'hd': {'$gte': data['hd']},
-            'usando': ['False']
+            'usando': 'False'
         }
     )
-    for resultados in busca:
-        print(resultados['_id'])
+    
+    return bson.json_util.dumps(busca)
+
+@app.route('/cliente/reservar/<pid>', methods=['POST'])
+def reserve_vm(pid):
+    data = request.get_json()
+    mongo.db['vm'].update_one(
+        {
+            'vcpu':data['vcpu'],
+            'ram': data['ram'],
+            'hd': data['hd'],
+        }, {'$set': {'usando':'True', 'reserva':pid}}
+    )
+    
+    return jsonify({'Ok': True})
+
+@app.route('/cliente/consultar/<pid>', methods=['POST'])
+def consultar_vm(pid):
+    data = request.get_json()
+    busca = mongo.db['vm'].find(
+        {
+            'reserva':pid,
+            'usando':'True'
+        }
+    )
+    return bson.json_util.dumps(busca)
+
+@app.route('/cliente/liberar', methods=['POST'])
+def liberar_vm():
+    data = request.get_json()
+    mongo.db['vm'].update_one(
+        {
+            'vcpu':{'$gte': data['vcpu']},
+            'ram': {'$gte': data['ram']},
+            'hd': {'$gte': data['hd']},
+        }, {'$set': {'usando':'False', 'reserva':'0'}}
+    )
 
     return jsonify({'Ok': True})
 
